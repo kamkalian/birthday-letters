5f5b01bef48d586d066c6847d1d43eb063efca6f
kamkalian <kurm_de+github@pm.me>
2025-09-29 19:25:12 +0200

    Renamed fastapi to fast_api

--- pm2_fastapi.config.js
+++ pm2_fastapi.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "birthday-letters-api",
       script: "/home/ingrid/birthday-letters/.venv/bin/uvicorn",
-      args: "fastapi.main:app --host 0.0.0.0 --port 8000",
+      args: "fast_api.main:app --host 0.0.0.0 --port 8000",
       interpreter: "none",
       cwd: "/home/ingrid/birthday-letters",
       autorestart: true,
